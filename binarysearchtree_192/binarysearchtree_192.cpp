526fb26815b8c1ab4240dcdfa2f6b7f84b4233e4
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-30 18:07:54 +0700

    inorder left and right

--- binarysearchtree_192/binarysearchtree_192.cpp
+++ binarysearchtree_192/binarysearchtree_192.cpp
@@ -76,4 +76,12 @@ public:
 		if (ROOT == NULL)
 		{
 			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			inorder(ptr->leftchild);
+			cout << ptr->info << "";
+			inorder(ptr->rightchild);
+		}
+	}
 
