84194e0f442f28f7eb7dca25da27df159205715b
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-30 18:25:37 +0700

    int main

--- binarysearchtree_192/binarysearchtree_192.cpp
+++ binarysearchtree_192/binarysearchtree_192.cpp
@@ -114,5 +114,22 @@ public:
 			postorder(ptr->leftchild);
 			postorder(ptr->rightchild);
 			cout << ptr->info << " ";
+		}
+	}
+};
+
+int main()
+{
+	BinaryTree obj;
+	while (true)
+	{
+		cout << "\nMenu" << endl;
+		cout << "1.Implement insert operation" << endl;
+		cout << "2.Perform inorder traversal" << endl;
+		cout << "3.Perform preorder traversal" << endl;
+		cout << "4.Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+		
 
 
